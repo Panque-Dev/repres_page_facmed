ed4883eb4d589111fa1bc573516a1ea8a53f2065
HEctorLNuevo <hectorhugo.librado0015@alumno.cch.unam.mx>
2025-11-14 11:54:11 -0600

    actualización depa ciru práctico

--- main.js
+++ main.js
@@ -178,7 +178,8 @@
         "Farmacología": { display: "FAR", file: "FAR" },
         "Inmunología": { display: "INM", file: "INM" },
         "Microbiología y Parasitología": { display: "MyP", file: "MyP" },
-        "Introducción a la Cirugía": { display: "ICR", file: "ICR" },
+        "Introducción a la Cirugía Teórico": { display: "ICR", file: "ICR" },
+        "Introducción a la Cirugía Práctico": { display: "ICR", file: "ICR" },
         "Promoción de la Salud en el Ciclo de Vida": { display: "PCV", file: "PCV" }
     };
     const SUBJECT_COLORS = {
@@ -281,12 +282,19 @@
             { id: "2-IBC2-O2", subject: "Integración Básico Clínica II", type: "Segundo ordinario",officialDate: "2026-05-26", officialTime: "13:00" },
             { id: "2-IBC2-EX", subject: "Integración Básico Clínica II", type: "Extraordinario",   officialDate: "2026-06-08", officialTime: "11:00" },
 
-            /* ===== Intro Cirugía ===== */
-            { id: "2-ICR-P1", subject: "Introducción a la Cirugía", type: "Primer parcial",   officialDate: "2025-01-12", officialTime: "8:00" },
-            { id: "2-ICR-P2", subject: "Introducción a la Cirugía", type: "Segundo parcial",  officialDate: "2026-04-6", officialTime: "8:00" },
-            { id: "2-ICR-O1", subject: "Introducción a la Cirugía", type: "Primer ordinario", officialDate: "2026-04-27", officialTime: "8:00" },
-            { id: "2-ICR-O2", subject: "Introducción a la Cirugía", type: "Segundo ordinario",officialDate: "2026-05-21", officialTime: "12:00" },
-            { id: "2-ICR-EX", subject: "Introducción a la Cirugía", type: "Extraordinario",   officialDate: "2026-06-06", officialTime: "8:00" },
+            /* ===== Intro Cirugía Teórico ===== */
+            { id: "2-ICR-P1", subject: "Introducción a la Cirugía Teórico", type: "Primer parcial",   officialDate: "2025-01-12", officialTime: "8:00" },
+            { id: "2-ICR-P2", subject: "Introducción a la Cirugía Teórico", type: "Segundo parcial",  officialDate: "2026-04-6", officialTime: "8:00" },
+            { id: "2-ICR-O1", subject: "Introducción a la Cirugía Teórico", type: "Primer ordinario", officialDate: "2026-04-28", officialTime: "8:00" },
+            { id: "2-ICR-O2", subject: "Introducción a la Cirugía Teórico", type: "Segundo ordinario",officialDate: "2026-05-21", officialTime: "12:00" },
+            { id: "2-ICR-EX", subject: "Introducción a la Cirugía Teórico", type: "Extraordinario",   officialDate: "2026-05-29", officialTime: "8:00" },
+
+            /* ===== Intro Cirugía Práctico ===== */
+            { id: "2-CIR-P1-PR", subject: "Introducción a la Cirugía", type: "Primer parcial (Práctico)", officialDate: "2026-01-12", officialTime: "08:00", evaluationWeek: "2026-01-12 al 2026-01-16"},
+            { id: "2-CIR-P2-PR", subject: "Introducción a la Cirugía", type: "Segundo parcial (Práctico)", officialDate: "2026-04-06", officialTime: "08:00", evaluationWeek: "2026-04-06 al 2026-04-10"},
+            { id: "2-CIR-O1-PR", subject: "Introducción a la Cirugía", type: "Primer ordinario (Práctico)", officialDate: "2026-04-27", officialTime: "13:00", },
+            { id: "2-CIR-O2-PR", subject: "Introducción a la Cirugía", type: "Segundo ordinario (Práctico)", officialDate: "2026-05-21", officialTime: "12:00" },
+            { id: "2-CIR-EX-PR", subject: "Introducción a la Cirugía", type: "Extraordinario (Práctico)", officialDate: "2026-05-29", officialTime: "08:00" },
 
             /* ===== Promo ===== */
             { id: "2-PCSV-P1", subject: "Promoción de la Salud en el Ciclo de la Vida", type: "Primer parcial",   officialDate: "2025-11-18", officialTime: "09:00" },
