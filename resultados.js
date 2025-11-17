c09b2a5bfcebf86315e09b7604c820065b0a4a90
mauriciosvcq <75230866+mauriciosvcq@users.noreply.github.com>
2025-11-16 19:17:29 -0600

    resultados 7

--- resultados.js
+++ resultados.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // ===== Ajustes visibles =====
-  const TOTAL_RESPONSES = 180;
+  const TOTAL_RESPONSES = 192;
   const BAR_CONFIG = [
     { max: 82, target: 71 }, // primero
     { max: 65, target: 58 }  // segundo
